89b178b75173e05d41950f18428740999b5519bb
Glen Crawford <glencraw4d@gmail.com>
2020-01-06 21:25:10 +1100

    Train the model using the batch generator, then use the learned embeddings as weights to output and plot the most similar movies to a given one.

--- main.py
+++ main.py
@@ -3,16 +3,21 @@ import functools
 import operator
 import numpy as np
 import random
+import pandas as pd
+import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
 import training_data_generator
 
 random.seed(100)
 
-# training_data_generator.download_current_revision_article_partition_files_in_latest_wikipedia_dump()
-# training_data_generator.process_current_revision_article_partition_files()
+plt.style.use('fivethirtyeight')
+plt.rcParams['font.size'] = 15
 
 TRAINING_DATA_FILE_PATH = 'data/training_data.json'
+
+def reverse_index_lookup(index, value):
+  return list(index.keys())[list(index.values()).index(value)]
 
 # Builds a generator that yields batches of positive and negative examples each time it is called.
 # * Positive examples are randomly sampled true movie and link pairs; they have a label of 1.
@@ -42,6 +47,77 @@ def generate_batch(number_of_positive_examples = 50, negative_positive_sample_ra
 
     yield {'movie': batch[:, 0], 'link': batch[:, 1]}, batch[:, 2]
 
+# Extract and normalize embeddings "so that the dot product between two embeddings becomes the cosine similarity".
+def extract_weights_from_embedding_layer(model, layer_name):
+  layer = model.get_layer(layer_name)
+  weights = layer.get_weights()[0]
+  weights = weights / np.linalg.norm(weights, axis = 1).reshape((-1, 1))
+  return weights
+
+# Receives a movie or link and a set of embeddings, and returns the most similar entities.
+# Do this by computing the dot product between the entity queries and the embeddings, which is the the cosine similarity between two vectors.
+# With cosine similarity, higher numbers indicate entities that are closer together, with -1 the furthest apart and +1 closest together, so all we have to do to find the closest entities in the embedding space is sort by the dot products.
+def find_similar_entities(name, weights, number_of_results = 10, return_distances = False, plot = False):
+  index = training_data_movie_titles_index
+
+  try:
+    entity_distances = np.dot(weights, weights[index[name]])
+  except KeyError:
+    print(f'{name} not found in movie index.')
+    return
+
+  sorted_entity_distances = np.argsort(entity_distances)
+
+  # Optionally plot results.
+  if plot:
+    plot_similar_entities(name, index, number_of_results, entity_distances, sorted_entity_distances)
+    return None
+
+  # Find the most similar entities by taking the last n sorted distances.
+  closest_entities = sorted_entity_distances[-number_of_results:]
+
+  if return_distances:
+    return entity_distances, closest_entities
+
+  print(f'\n\nMovies closest to {name}.\n')
+
+  # Print formatting.
+  max_width = max([len(reverse_index_lookup(index, entity)) for entity in closest_entities])
+
+  # Print the most similar entitites and their distances.
+  for entity in reversed(closest_entities):
+    print(f'Movie: {reverse_index_lookup(index, entity):{max_width + 2}} Similarity: {entity_distances[entity]:.{2}}')
+
+def plot_similar_entities(name, index, number_of_results, entity_distances, sorted_entity_distances):
+  # Find furthest and closest entities.
+  furthest_entities = sorted_entity_distances[:(number_of_results // 2)]
+  closest_entities = sorted_entity_distances[-number_of_results-1: len(entity_distances) - 1]
+
+  entities_to_plot = [reverse_index_lookup(index, entity) for entity in furthest_entities]
+  entities_to_plot.extend(reverse_index_lookup(index, entity) for entity in closest_entities)
+
+  # Get the distances of the entities to plot.
+  distances = [entity_distances[entity] for entity in furthest_entities]
+  distances.extend(entity_distances[entity] for entity in closest_entities)
+
+  colors = ['r' for _ in range(number_of_results // 2)]
+  colors.extend('g' for _ in range(number_of_results))
+
+  data = pd.DataFrame({'distance': distances}, index = entities_to_plot)
+
+  # Plot a horizontal bar chart.
+  data['distance'].plot.barh(color = colors, figsize = (10, 8), edgecolor = 'k', linewidth = 2)
+  plt.xlabel('Cosine Similarity')
+  plt.axvline(x = 0, color = 'k')
+
+  chart_name = f'Movies Most and Least Similar to'
+  for word in name.split():
+    chart_name += ' $\it{' + word + '}$'
+  plt.title(chart_name, x = 0.2, size = 28, y = 1.05)
+  plt.show()
+
+### Load, index and combine the training data ###
+
 with open(TRAINING_DATA_FILE_PATH, 'r') as training_data_file:
   training_data = json.load(training_data_file)['movies']
 
@@ -59,32 +135,40 @@ for movie in training_data:
   for link in movie['internal_links']:
     movie_link_pairs.append((training_data_movie_titles_index[movie['title']], training_data_all_links_index[link]))
 
-## Define the model
-# classification = False
-# Both inputs are 1-dimensional
-movie = keras.layers.Input(name = 'movie', shape = [1])
-link = keras.layers.Input(name = 'link', shape = [1])
+training_data_batch_generator = generate_batch(number_of_positive_examples = 1024, negative_positive_sample_ratio = 2.0)
 
-# Embedding the movie (shape will be (None, 1, 50))
-movie_embedding = keras.layers.Embedding(name = 'movie_embedding', input_dim = len(training_data), output_dim = 50)(movie)
-# Embedding the link (shape will be (None, 1, 50))
-link_embedding = keras.layers.Embedding(name = 'link_embedding', input_dim = len(training_data_all_links), output_dim = 50)(link)
+### Define the model ###
 
-# Merge the layers with a dot product along the second axis (shape will be (None, 1, 1))
-merged = keras.layers.Dot(name = 'dot_product', normalize = True, axes = 2)([movie_embedding, link_embedding])
+# Inputs (both are one-dimensional).
+movie_input = keras.layers.Input(name = 'movie', shape = [1])
+link_input = keras.layers.Input(name = 'link', shape = [1])
 
-# Reshape to be a single number (shape will be (None, 1))
-merged = keras.layers.Reshape(target_shape = [1])(merged)
+# Embeddings for the movies (shape will be (None, 1, 50)).
+movie_embedding = keras.layers.Embedding(name = 'movie_embedding', input_dim = len(training_data_movie_titles_index), output_dim = 50)(movie_input)
 
-# Squash outputs for classification
-out = keras.layers.Dense(1, activation = 'sigmoid')(merged)
-model = Model(inputs = [movie, link], outputs = out)
+# Embeddings for the links (shape will be (None, 1, 50)).
+link_embedding = keras.layers.Embedding(name = 'link_embedding', input_dim = len(training_data_all_links_index), output_dim = 50)(link_input)
 
-# Compile using specified optimizer and loss 
-model.compile(
-  optimizer = 'Adam',
-  loss = 'binary_crossentropy',
-  metrics = ['accuracy']
-)
+# Merge the layers with a dot product along the second axis (shape will be (None, 1, 1)).
+# Then reshape to be a single number (shape will be (None, 1)).
+merged_output = keras.layers.Dot(name = 'dot_product', normalize = True, axes = 2)([movie_embedding, link_embedding])
+merged_output = keras.layers.Reshape(target_shape = [1])(merged_output)
+
+model = keras.Model(inputs = [movie_input, link_input], outputs = merged_output)
+model.compile(optimizer = 'Adam', loss = 'mse')
 
 model.summary()
+
+### Train! ###
+
+model.fit_generator(
+  training_data_batch_generator,
+  epochs = 3, # 15
+  steps_per_epoch = (len(movie_link_pairs) // 1024),
+  verbose = 2
+)
+
+### Inspect the results ###
+
+movie_weights = extract_weights_from_embedding_layer(model, 'movie_embedding')
+find_similar_entities('Blade Runner', movie_weights, plot = False)
