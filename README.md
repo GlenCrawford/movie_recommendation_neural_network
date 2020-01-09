6a5351d6d3c5a8f32ed28e127fdbbc03e17c80a6
Glen Crawford <glencraw4d@gmail.com>
2020-01-09 19:58:27 +1100

    Allow the movie to query to be passed in as an argument from the command line, and last tidy ups of the README and main script.

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # Movie Recommendation Neural Network Based on Learned Embeddings From Wikipedia Articles
 
-This is a Tensorflow 2 and Keras neural network project to make movie recommendations for a specified movie using Neural Network Embeddings trained on all articles about movies in the English language Wikipedia. Training the model on this source is predicated on the principle that movies which link to similar Wikipedia pages are therefore similar to one another themselves. As a result, this model computes their similarity by learning "embeddings" of movies and their internal Wikipedia links as a 50-dimensional vector.
+This is a Tensorflow 2 and Keras neural network project to make movie recommendations for a specified movie using Neural Network Embeddings trained on all articles about movies in the English language Wikipedia. Training the model on this source is predicated on the principle that movies which link to similar Wikipedia pages are therefore similar to one another themselves. To achieve this, this model computes their similarity by learning "embeddings" of movies and their internal Wikipedia links as a 50-dimensional vector.
 
 ## How it works
 
-To start with, the `training_data_generator.py` file downloads the current revision of _all Wikipedia articles_ in the latest Wikipedia data dump from [https://dumps.wikimedia.org/enwiki/latest/](https://dumps.wikimedia.org/enwiki/latest/) as compressed partition files. As of the 5th of January 2020, that equates to 59 files each around 300–400 MB totalling about 16 GB compressed, and expanding to over 60 GB when decompressed! It then processes each of these files, collecting all articles about movies, extracts their titles and links, and finally saves it all as a JSON file called `data/training_data.json`.
+To start with, the `training_data_generator.py` file downloads the current revision of _all Wikipedia articles_ in the latest Wikipedia data dump from [https://dumps.wikimedia.org/enwiki/latest/](https://dumps.wikimedia.org/enwiki/latest/) as compressed partition files. As of the 5th of January 2020, that equates to 59 files, each around 300–400 MB totalling about 16 GB compressed, and expanding to over 60 GB when decompressed! It then processes each of these files, collecting all articles about movies, extracting their titles and links, and finally saving it all as a JSON file called `data/training_data.json`.
 
 > "Embeddings are a way to represent discrete — categorical — variables as continuous vectors. In contrast to an encoding method like one-hot encoding, neural network embeddings are low-dimensional and learned, which means they place similar entities closer to one another in the embedding space."
 
-The input to the model is batches of movie and link (as index integers) examples, where the real (positive) examples are randomly sampled from the training data and have a label of 1, and the false (negative) examples have a label of -1. The output is a prediction of whether or not the link was indeed present in the movie's Wikipedia article. While that is the output of the model, it's just for training; we're not actually interested in the prediction, what we're really after is the learned embeddings. In other words, whereas with most neural networks the weights are how we make the output predictions, in this network it's the other way around; the weights (the embeddings) are the output, and the predictions are how the model learns them.
+The input to the model is batches of movie and link pairs (as index integers), where the real (positive) examples are randomly sampled from the training data and have a label of 1, and the false (negative) examples have a label of -1. The output is a prediction of whether or not the link was indeed present in the movie's Wikipedia article. While that is the output of the model, it's just for training; we're not actually interested in the prediction, what we're really after is the learned embeddings. In other words, whereas with most neural networks the weights are how we make the output predictions, in this network it's the other way around; the weights (the embeddings) are the output, and the predictions are how the model learns them.
 
-While training, the model learns similar embeddings for movies that link to similar articles and thus places similar movies next to one another in the embedding space. Once trained, we extract and normalize the embeddings from the embedding layers, calculate the cosine similarity between the embeddings for all movies and the embeddings for the movie that we are querying, and sort them (higher numbers indicate entities that are closer together, with -1 being the furthest apart and +1 being the closest together) to rank each movie by similarity to the query movie (in other words, find the closest neighbours in the embedding space).
+While training, the model learns similar embeddings for movies that link to similar articles and thus places similar movies next to one another in the embedding space. Once trained, we extract and normalize the embeddings from the embedding layers, calculate the cosine similarity between the embeddings for all movies and the embeddings for the movie that we are querying, and sort them (higher numbers indicate entities that are closer together, with -1 being the furthest apart and 1 being the closest together) to rank each movie by similarity to the query movie (in other words, find the closest neighbours in the embedding space).
 
 This project is adapted from a [notebook](https://github.com/WillKoehrsen/wikipedia-data-science/blob/master/notebooks/Book%20Recommendation%20System.ipynb) by [Will Koehrsen](http://twitter.com/@koehrsen_will).
 
@@ -53,8 +53,29 @@ python -W ignore training_data_generator.py
 ### Build and train the model and output results
 
 ```bash
-python -W ignore main.py
+python -W ignore main.py "Blade Runner"
 ```
+
+If you want to view a horizontal bar chart of the results, set `plot = True` in the call to `find_similar_entities()`.
+
+The above command will output a summary of the model's architecture and the progress of the training, and then print results that will look like the following:
+
+```
+Movies most similar to Blade Runner:
+
+Movie: Blade Runner                         Similarity: 1.0
+Movie: Blade Runner 2049                    Similarity: 0.98
+Movie: Alien                                Similarity: 0.94
+Movie: 2001: A Space Odyssey                Similarity: 0.93
+Movie: A Clockwork Orange                   Similarity: 0.93
+Movie: Jurassic Park                        Similarity: 0.91
+Movie: The Shining                          Similarity: 0.90
+Movie: The Terminator                       Similarity: 0.88
+Movie: The Big Lebowski                     Similarity: 0.88
+Movie: Indiana Jones and the Last Crusade   Similarity: 0.87
+```
+
+The fact that the queried movie is at the very top of the list with a similarity of exactly 1.0 means that it's working!
 
 ## Monitoring/logging
 
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from sys import argv
 import json
 import functools
 import operator
@@ -10,20 +11,16 @@ import tensorflow as tf
 from tensorflow import keras
 import training_data_generator
 
-random.seed(100)
-
-plt.style.use('fivethirtyeight')
-plt.rcParams['font.size'] = 15
-
 TRAINING_DATA_FILE_PATH = 'data/training_data.json'
 LOG_DIRECTORY = 'logs/fit/' + datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
 
+# Not really how dictionaries are meant to be used, but better than building a second (reversed) dictionary for each index.
 def reverse_index_lookup(index, value):
   return list(index.keys())[list(index.values()).index(value)]
 
 # Builds a generator that yields batches of positive and negative examples each time it is called.
 # * Positive examples are randomly sampled true movie and link pairs; they have a label of 1.
-# * Negative examples, are randomly sampled false movie and link pairs; they have a label of -1.
+# * Negative examples are randomly sampled false movie and link pairs; they have a label of -1.
 def generate_batch(number_of_positive_examples = 50, negative_positive_sample_ratio = 1.0):
   batch_size = number_of_positive_examples * (1 + negative_positive_sample_ratio)
   batch = np.zeros((int(batch_size), 3))
@@ -49,7 +46,7 @@ def generate_batch(number_of_positive_examples = 50, negative_positive_sample_ra
 
     yield {'movie': batch[:, 0], 'link': batch[:, 1]}, batch[:, 2]
 
-# Extract and normalize embeddings "so that the dot product between two embeddings becomes the cosine similarity".
+# Extract and normalize embeddings from the specified layer.
 def extract_weights_from_embedding_layer(model, layer_name):
   layer = model.get_layer(layer_name)
   weights = layer.get_weights()[0]
@@ -58,12 +55,10 @@ def extract_weights_from_embedding_layer(model, layer_name):
 
 # Receives a movie or link and a set of embeddings, and returns the most similar entities.
 # Do this by computing the dot product between the entity queries and the embeddings, which is the the cosine similarity between two vectors.
-# With cosine similarity, higher numbers indicate entities that are closer together, with -1 the furthest apart and +1 closest together, so all we have to do to find the closest entities in the embedding space is sort by the dot products.
-def find_similar_entities(name, weights, number_of_results = 10, return_distances = False, plot = False):
-  index = training_data_movie_titles_index
-
+# With cosine similarity, higher numbers indicate entities that are closer together, with -1 the furthest apart and +1 closest together, so all we have to do to find the closest/furthest entities in the embedding space is sort by the dot products.
+def find_similar_entities(name, weights, number_of_results = 10, plot = False):
   try:
-    entity_distances = np.dot(weights, weights[index[name]])
+    entity_distances = np.dot(weights, weights[training_data_movie_titles_index[name]])
   except KeyError:
     print(f'{name} not found in movie index.')
     return
@@ -72,31 +67,20 @@ def find_similar_entities(name, weights, number_of_results = 10, return_distance
 
   # Optionally plot results.
   if plot:
-    plot_similar_entities(name, index, number_of_results, entity_distances, sorted_entity_distances)
-    return None
+    plot_similar_entities(name, number_of_results, entity_distances, sorted_entity_distances)
 
   # Find the most similar entities by taking the last n sorted distances.
   closest_entities = sorted_entity_distances[-number_of_results:]
 
-  if return_distances:
-    return entity_distances, closest_entities
+  return entity_distances, closest_entities
 
-  print(f'\n\nMovies closest to {name}.\n')
-
-  # Print formatting.
-  max_width = max([len(reverse_index_lookup(index, entity)) for entity in closest_entities])
-
-  # Print the most similar entitites and their distances.
-  for entity in reversed(closest_entities):
-    print(f'Movie: {reverse_index_lookup(index, entity):{max_width + 2}} Similarity: {entity_distances[entity]:.{2}}')
-
-def plot_similar_entities(name, index, number_of_results, entity_distances, sorted_entity_distances):
+def plot_similar_entities(name, number_of_results, entity_distances, sorted_entity_distances):
   # Find furthest and closest entities.
   furthest_entities = sorted_entity_distances[:(number_of_results // 2)]
   closest_entities = sorted_entity_distances[-number_of_results-1: len(entity_distances) - 1]
 
-  entities_to_plot = [reverse_index_lookup(index, entity) for entity in furthest_entities]
-  entities_to_plot.extend(reverse_index_lookup(index, entity) for entity in closest_entities)
+  entities_to_plot = [reverse_index_lookup(training_data_movie_titles_index, entity) for entity in furthest_entities]
+  entities_to_plot.extend(reverse_index_lookup(training_data_movie_titles_index, entity) for entity in closest_entities)
 
   # Get the distances of the entities to plot.
   distances = [entity_distances[entity] for entity in furthest_entities]
@@ -116,7 +100,15 @@ def plot_similar_entities(name, index, number_of_results, entity_distances, sort
   for word in name.split():
     chart_name += ' $\it{' + word + '}$'
   plt.title(chart_name, x = 0.2, size = 28, y = 1.05)
+
+  plt.style.use('fivethirtyeight')
+  plt.rcParams['font.size'] = 15
+
   plt.show()
+
+if len(argv) <= 1:
+  print('You must pass in a movie name as an argument. Please consult the README.')
+  exit()
 
 ### Load, index and combine the training data ###
 
@@ -137,11 +129,9 @@ for movie in training_data:
   for link in movie['internal_links']:
     movie_link_pairs.append((training_data_movie_titles_index[movie['title']], training_data_all_links_index[link]))
 
-training_data_batch_generator = generate_batch(number_of_positive_examples = 1024, negative_positive_sample_ratio = 2.0)
-
 ### Define the model ###
 
-# Inputs (both are one-dimensional).
+# Inputs (both one-dimensional).
 movie_input = keras.layers.Input(name = 'movie', shape = [1])
 link_input = keras.layers.Input(name = 'link', shape = [1])
 
@@ -159,19 +149,34 @@ merged_output = keras.layers.Reshape(target_shape = [1])(merged_output)
 model = keras.Model(inputs = [movie_input, link_input], outputs = merged_output)
 model.compile(optimizer = 'Adam', loss = 'mse')
 
+print('\nModel architecture:')
+
 model.summary()
 
 ### Train! ###
 
+print('\nTraining the model...')
+
 model.fit_generator(
-  training_data_batch_generator,
-  epochs = 3, # 15
+  generate_batch(number_of_positive_examples = 1024, negative_positive_sample_ratio = 2.0),
+  epochs = 15,
   steps_per_epoch = (len(movie_link_pairs) // 1024),
   verbose = 2,
   callbacks = [tf.keras.callbacks.TensorBoard(log_dir = LOG_DIRECTORY)]
 )
 
-### Inspect the results ###
+### Inspect and output the results ###
+
+movie_query = argv[1]
 
 movie_weights = extract_weights_from_embedding_layer(model, 'movie_embedding')
-find_similar_entities('Blade Runner', movie_weights, plot = False)
+entity_distances, closest_entities = find_similar_entities(movie_query, movie_weights, plot = False)
+
+print(f'\nMovies most similar to {movie_query}:\n')
+
+# Output formatting.
+max_width = max([len(reverse_index_lookup(training_data_movie_titles_index, entity)) for entity in closest_entities])
+
+# Print the most similar entitites and their distances.
+for entity in reversed(closest_entities):
+  print(f'Movie: {reverse_index_lookup(training_data_movie_titles_index, entity):{max_width + 2}} Similarity: {entity_distances[entity]:.{2}}')
